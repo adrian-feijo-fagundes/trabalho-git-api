916ecdf2c1dc61687d8706bfe899c1a158f4f15e
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-11-13 19:39:00 -0300

    Criando tabela de usuarios

--- src/db/database.js
+++ src/db/database.js
@@ -6,5 +6,22 @@ const db = new sqlite3.Database('./db/ecommerce.db', (err) => {
     console.log('Connected to SQLite database');
   }
 });
+db.serialize(() => {
+    db.run(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      )
+    `, (err) => {
+      if (err) {
+        console.error('Error creating users table:', err);
+      } else {
+        console.log('Users table created');
+      }
+    });
+  });
+  
 
 module.exports = db;
