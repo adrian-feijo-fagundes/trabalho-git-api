6f21b8f06df486a4c34e113f41d212db51061a99
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-11-13 19:33:52 -0300

    Fazendo a conexão inicial com o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const db = require('./db/database');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
